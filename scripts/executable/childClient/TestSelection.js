85e427e08005306d10e72ee1185fa9098944fc57
ilvas <il.vsl0110@gmail.com>
2020-11-14 15:24:22 +0300

    fix bugs

--- scripts/executable/childClient/TestSelection.js
+++ scripts/executable/childClient/TestSelection.js
@@ -4,12 +4,15 @@ const path = require('path');
 
 const os = require('os');
 
-fs.readdir(path.join(process.cwd(), 'Tests'), (err, files) => {
+if (!fs.existsSync('../../Users')) fs.mkdir('../../Users');
+if (!fs.existsSync('../../Tests')) fs.mkdir('../../Tests');
+
+fs.readdir(path.join('../../Tests'), (err, files) => {
   f = files.map((e) => {
     console.log(e); 
     return {
       name: e,
-      time: fs.statSync(path.join(process.cwd(), 'Tests', e)).mtime.getTime()
+      time: fs.statSync(path.join('../../Tests', e)).mtime.getTime()
     };
   }).sort((a, b) => {
     return a.time - b.time;
@@ -19,17 +22,17 @@ fs.readdir(path.join(process.cwd(), 'Tests'), (err, files) => {
     elToCreate.className = 'text-center d-flex p-2';
     elToCreate.innerHTML = '<button type="next" class="btn btn-primary btn-test btn-block">'+e+'</button>';
     elToCreate.onclick = () => {
-      fs.mkdir(path.join(process.cwd(), 'Users'), (err)=>{});
+      fs.mkdir(path.join('../..Users'), (err)=>{});
       //fs.mkdir(path.join(process.cwd()), (e)=>{})
       nameInp = document.getElementById('NameInput');
       myJSON = [{test:e, name:nameInp.value}];
-      if (fs.existsSync(path.join(process.cwd(), 'Users', os.hostname+'.json'))) {
-        myJSON = require(path.join(process.cwd(), 'Users', os.hostname+'.json'));
+      if (fs.existsSync(path.join('../..Users', os.hostname+'.json'))) {
+        myJSON = require(path.join('../../Users', os.hostname+'.json'));
         myJSON.push({test:e, name:nameInp.value});
       }
-      fs.writeFile(path.join(process.cwd(), 'Users', os.hostname+'.json'), JSON.stringify(myJSON), (e)=>{});      
-      fs.mkdir(path.join(process.cwd(), 'Users', nameInp.value+e), (e)=>{});
-      window.location.href = path.join(process.cwd(), 'templates/childClient/First.html');
+      fs.writeFile(path.join('../..Users', os.hostname+'.json'), JSON.stringify(myJSON), (e)=>{});      
+      fs.mkdir(path.join('../../Users', nameInp.value+e), (e)=>{});
+      window.location.href = path.join('../../templates/childClient/First.html');
     }
     document.getElementById('container').insertBefore(elToCreate, pos);
   });
